9ec0dc0b61d15cfc458592b200433f47a1316352
Tom <32821177+tomtactom@users.noreply.github.com>
2021-10-22 22:32:08 +0200

    .

---
+++ index.php
@@ -0,0 +1,46 @@
+<?php
+/*
+
+*/
+
+?>
+<html lang="de">
+<head>
+  <meta charset="utf-8">
+  <title>Segelcamp 2021 | Seglergemeinschaft Lohheider See e.V.</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no">
+  <meta name="language" content="de">
+  <meta name="audience" content="experiment">
+  <meta name="page-type" content="Formular">
+  <meta name="robots" content="noindex, nofollow">
+  <meta http-equiv="language" content="german, de">
+  <meta name="author" content="Tom Aschmann">
+  <meta name="copyright" content="Tom Aschmann">
+  <meta name="publisher" content="Hochschule Rhein-Waal">
+  <meta name="date" content="2021-10-22">
+	<link rel="stylesheet" href="./main.css">
+
+</head>
+<body>
+  <article>
+    <form class="" action="index.html" method="post">
+
+
+      <div class="question">
+        <label>
+          <input type="text" placeholder="" minlength="" maxlength="" required>
+        </label>
+      </div>
+
+      <div class="question">
+        <label>
+          <input type="range" min="1" max="5" step="1.0" value="3">
+         </label>
+      </div>
+
+
+
+    </form>
+  </article>
+</body>
+</html>
